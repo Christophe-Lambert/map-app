45c68c697057008eb8df734e99f10fb10fb1c585
Christophe Lambert <christophelambert@yahoo.com>
2025-01-02 20:46:44 +0100

    Add table locations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MapComponent } from './map/map.component';
 import { HttpClientModule } from '@angular/common/http';
+import { LocationsTableComponent } from './locations-table/locations-table.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HttpClientModule,  MapComponent],
+  imports: [RouterOutlet, HttpClientModule,  MapComponent, LocationsTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/locations-table/locations-table.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { LocationService } from '../services/location.service';
+import { Location } from '../models/location.model';
+
+@Component({
+  selector: 'app-locations-table',
+  standalone: true,
+  imports: [MatTableModule,
+    MatPaginatorModule,
+    MatSortModule],
+  providers: [LocationService],
+  templateUrl: './locations-table.component.html',
+  styleUrl: './locations-table.component.css'
+})
+export class LocationsTableComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'name', 'latitude', 'longitude'];
+  dataSource = new MatTableDataSource<Location>();
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+
+  constructor(private locationService: LocationService) { }
+
+  ngOnInit(): void {
+    this.locationService.getAllLocations().subscribe({
+      next: (locations: Location[]) => {
+        this.dataSource.data = locations;
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      },
+      error: (error) => {
+        console.error('Erreur lors de la récupération des locations:', error);
+      }
+    });
+  }
+
+  applyFilter(event: Event): void {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+}
